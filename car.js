55848439878ec785d54d113aaaaed1f4c863b0f0
kenan-erol <kenan.erol@yale.edu>
2023-01-25 21:16:42 -0500

    Update car.js
    Implemented friction
    car reverse driving mechanics

--- car.js
+++ car.js
@@ -7,6 +7,9 @@ class Car {
 
         this.speed=0;
         this.acceleration=0.15;
+        this.maxSpeed=5;
+        //friction
+        this.friction=0.05;
 
 
         this.controls = new Controls();
@@ -17,9 +20,43 @@ class Car {
         if(this.controls.forward){
             this.speed+=this.acceleration;
         }
+
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
+
+        if(this.speed>this.maxSpeed){
+            // limit car speed
+            this.speed=this.maxSpeed;
+        }
+
+        if(this.speed<-this.maxSpeed/2){
+            // cut the speed of the car in half when in reverse
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+
+        if(Math.abs(this.speed)<this.friction){
+            // fix friction movement issue
+            this.speed=0;
+        }
+
+        // movement in the x axis
+        if(this.controls.left){
+            this.x-=2;
+        }
+
+        if(this.controls.right){
+            this.x+=2;
+        }
+
         this.y-=this.speed;
     }
     
